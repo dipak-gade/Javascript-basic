b118ed1879a6c6fa2e0c0ac34d8600b697ac5854
dipak-gade <gadedipak9000@gmail.com>
2025-02-06 15:59:51 +0530

    Classes

--- sample.js
+++ sample.js
@@ -1,3 +1,17 @@
-console.log("This is sample file");
-console.log("Hello World");
-console.log("JavaScript");
+class Person{
+    eat(){
+        console.log("eating");
+    };
+
+    sleep(){
+        console.log("sleeping");
+    }
+}
+
+class Doctor{
+    work(){
+        console.log("treat a patient");
+    }
+}
+
+const doctorObj = new Person;
